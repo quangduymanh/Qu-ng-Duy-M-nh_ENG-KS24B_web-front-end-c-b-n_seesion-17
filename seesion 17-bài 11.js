bde3b46edb4df038de28b02029929dc4be9454c5
quangduymanh <quangduymanh4@gmail.com>
2025-02-27 19:35:49 +0700

    upload bài 11

---
+++ seesion 17-bài 11.js
@@ -0,0 +1,11 @@
+document.write(`<p>${Math.abs(-5.5)}</p>`); 
+document.write(`<p>${Math.abs(10)}</p>`); 
+
+document.write(`<p>${Math.ceil(4.4)}</p>`); 
+document.write(`<p>${Math.ceil(5.8)}</p>`);
+
+console.log(Math.floor(5.7));
+console.log(Math.floor(6.1));
+
+console.log(Math.round(10.1));
+console.log(Math.round(20.6));
